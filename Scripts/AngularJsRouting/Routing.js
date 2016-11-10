c695642a8a8cd0cca54e503c523e0c4342b8a5e2
nishan balasooriya <nishannnb@gmail.com>
2016-11-10 11:06:09 +0530

    1

---
+++ Scripts/AngularJsRouting/Routing.js
@@ -0,0 +1,30 @@
+﻿var app = angular.module('MyApp', ['ngRoute']);
+app.config([
+    '$locationProvider', '$routeProvider',
+    function ($locationProvider, $routeProvider) {
+
+        $routeProvider
+        .when('/Home', { // For Home Page
+            templateUrl: '/AngularTemplates/Home.html',
+            controller: 'HomeController'
+         })
+        .when('/Contact', { // For Contact page
+            templateUrl: '/AngularTemplates/Contact.html',
+            controller: 'ContactController'
+        })
+        .when('/About', { // For About page
+            templateUrl: '/AngularTemplates/About.html',
+            controller: 'AboutController'
+        })
+        .when('/User/:userid', { // For User page 
+           templateUrl: '/AngularTemplates/User.html',
+           controller: 'UserController'
+        })
+        .otherwise({   // This is when any route not matched => error
+           controller: 'ErrorController'
+        })
+    }]);
+
+
+
+
